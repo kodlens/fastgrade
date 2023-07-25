5b742eab1f5948fa3775cfccf6472c303efea459
etienne <etiennewayne@gmail.com>
2023-07-25 08:55:49 +0800

    update repo

--- public/js/app.js
+++ public/js/app.js
@@ -28204,7 +28204,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\n.card-width[data-v-4f01a62c]{\n    width: 640px;\n}\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\n.card-width[data-v-4f01a62c]{\r\n    width: 640px;\n}\r\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
---
+++ storage/framework/views/1273626f33ab68ff2f74981d245f505e063fc78d.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+
+    <title><?php echo e(config('app.name', 'Laravel')); ?></title>
+
+    <!-- Scripts -->
+    <script src="<?php echo e(asset('js/app.js')); ?>" defer></script>
+
+    <!-- Fonts -->
+
+
+
+    <!-- Styles -->
+    <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet">
+
+
+</head>
+<body>
+    <div id="app">
+
+        <div>
+            <?php echo $__env->yieldContent('content'); ?>
+        </div>
+
+
+    </div>
+</body>
+</html>
+<?php /**PATH C:\Users\etien\OneDrive\Desktop\Github Proj\fastgrade\resources\views/layouts/no-navbar.blade.php ENDPATH**/ ?>
